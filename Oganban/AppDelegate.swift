e999ad02fc64c68c8e71a5ce283cedc28b40ba1b
Coby <coby@vinova.sg>
2018-12-25 18:55:53 +0700

    Add cell

--- Oganban/AppDelegate.swift
+++ Oganban/AppDelegate.swift
@@ -16,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let vc = MyBuyRouter.createModule()
+        
+        window?.rootViewController = vc
+        
         return true
     }
 
