9db39205b3c87d45e16dc62172cbb1b771ded025
dinhtien <dvtien@lacviet.com.vn>
2019-01-12 14:18:18 +0700

    Init comment detail

--- Oganban/Services/APIService/NotificationAPIService.swift
+++ Oganban/Services/APIService/NotificationAPIService.swift
@@ -11,9 +11,15 @@ import Foundation
 import Foundation
 
 protocol NotificationAPIServiceProtocol {
-    func getNotification(offset: Int, success: @escaping SuccessHandler<ParentNotificationEntity>.object, failure: @escaping RequestFailure)
+    func getNotification(offset: Int, limit: Int?, success: @escaping SuccessHandler<ParentNotificationEntity>.object, failure: @escaping RequestFailure)
     func readNotification(notificationId: String, success: @escaping SuccessHandler<BaseEntity>.object, failure: @escaping RequestFailure)
     
+}
+
+extension NotificationAPIServiceProtocol {
+    func getNotification(offset: Int, limit: Int? = nil, success: @escaping SuccessHandler<ParentNotificationEntity>.object, failure: @escaping RequestFailure) {
+        return getNotification(offset: offset, limit: limit, success: success, failure: failure)
+    }
 }
 
 class NotificationAPIService: NotificationAPIServiceProtocol {
@@ -24,8 +30,8 @@ class NotificationAPIService: NotificationAPIServiceProtocol {
         self.network = network
     }
     
-    func getNotification(offset: Int, success: @escaping SuccessHandler<ParentNotificationEntity>.object, failure: @escaping RequestFailure) {
-        let endPoint = NotificationEndPoint.getNotification(offset: offset)
+    func getNotification(offset: Int, limit: Int? = nil, success: @escaping SuccessHandler<ParentNotificationEntity>.object, failure: @escaping RequestFailure) {
+        let endPoint = NotificationEndPoint.getNotification(offset: offset, limit: limit)
         network.requestData(endPoint: endPoint, success: MapperData.mapObject(success), failure: failure)
     }
     
