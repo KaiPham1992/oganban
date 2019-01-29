4cd65340d70874f49585db9daf5976cc6601573c
Kai Pham <kai@vinova.sg>
2019-01-30 00:45:08 +0700

    fix notification

--- Oganban/AppRouter.swift
+++ Oganban/AppRouter.swift
@@ -53,31 +53,42 @@ class AppRouter {
     }
     
     func handleNotification(userInfo: [AnyHashable : Any]) {
-        guard let aps = userInfo["aps"] as? [String: Any]  else { return }
-        guard let topVC = UIApplication.topViewController() else { return }
-//        guard let key = aps["screen"] as? String else { return }
-//        guard let id = aps["obj_id"] as? String else { return }
-        let id = "142"
-        let key = "ORDER_BUYER"
-        let json = JSON(aps)
-        print(json)
         
-        
-        switch key {
-        case "ORDER_BUYER":
-            let vc = MyBuyDetailRouter.createModule(orderId: id, isSaler: false)
-            topVC.push(controller: vc)
-        case "ORDER_SELLER":
-            let vc = MyBuyDetailRouter.createModule(orderId: id, isSaler: true)
-            topVC.push(controller: vc)
-        case "RE_COMMENT":
-            let vc = CommentDetailRouter.createModule(recordId: id)
-            topVC.push(controller: vc)
-        case "COMMENT":
-            let vc = CommentDetailRouter.createModule(recordId: id)
-            topVC.push(controller: vc)
-        default:
-           break
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.2) {
+            guard let aps = userInfo["aps"] as? [String: Any]  else { return }
+            guard let topVC = UIApplication.topViewController() else { return }
+            guard let key = aps["screen"] as? String else { return }
+            guard let id = aps["obj_id"] as? String else { return }
+            guard let idNotification = aps["_id"] as? String else { return }
+            
+            Provider.shared.notificationAPIService.readNotification(notificationId: idNotification, success: { _ in
+                
+            }, failure: { _ in
+                
+            })
+            // Test
+            //let id = "142"
+            //let key = "ORDER_BUYER"
+            let json = JSON(aps)
+            print(json)
+            
+            
+            switch key {
+            case "ORDER_BUYER":
+                let vc = MyBuyDetailRouter.createModule(orderId: id, isSaler: false)
+                topVC.push(controller: vc)
+            case "ORDER_SELLER":
+                let vc = MyBuyDetailRouter.createModule(orderId: id, isSaler: true)
+                topVC.push(controller: vc)
+            case "RE_COMMENT":
+                let vc = CommentDetailRouter.createModule(recordId: id)
+                topVC.push(controller: vc)
+            case "COMMENT":
+                let vc = CommentDetailRouter.createModule(recordId: id)
+                topVC.push(controller: vc)
+            default:
+                break
+            }
         }
     }
 }
