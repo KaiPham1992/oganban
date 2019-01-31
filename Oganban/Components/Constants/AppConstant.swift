def0ba0e14410664f9c3c1c2b3c20a1ff4dcbb6f
Kai Pham <kai@vinova.sg>
2019-02-01 02:20:10 +0700

    fix admob

--- Oganban/Components/Constants/AppConstant.swift
+++ Oganban/Components/Constants/AppConstant.swift
@@ -10,8 +10,8 @@ import UIKit
 
 struct AppConstant {
     #if APPSTORE
-    static let admobNativeAds = "ca-app-pub-8064036299806325~8781055145"
-    static let admobBanner = "ca-app-pub-8064036299806325~8781055145"
+    static let admobNativeAds = "ca-app-pub-8064036299806325/9836354585"//"ca-app-pub-8064036299806325/9836354585"
+    static let admobBanner = "ca-app-pub-8064036299806325/9836354585"
     #else
     static let admobNativeAds = "ca-app-pub-3940256099942544/3986624511"
     static let admobBanner = "ca-app-pub-3940256099942544/2934735716"
